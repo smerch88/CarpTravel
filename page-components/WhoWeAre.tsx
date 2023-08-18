1a57995ce3e646a9513619b87a7a4b879c026037
smerch88 <maxarmyk@gmail.com>
2023-08-19 02:16:59 +0300

    feat: mobile adaptive add

--- page-components/WhoWeAre.tsx
+++ page-components/WhoWeAre.tsx
@@ -7,26 +7,34 @@ export const WhoWeAre = () => {
         <h2 className="uppercase text-title mb-6 md:mb-0 md:text-titleMd xl:text-titleXl">
           WHO <span className="font-medium">WE ARE</span>
         </h2>
-        <p className="max-w-[180px] mb-10">
+        <p className="max-w-[180px] mb-10 text-sm font-extralight md:mb-7 md:text-base leading-[20px] xl:text-lg xl:leading-[24px]">
           <span className="mb-5 block">
-            a team of enthusiasts who are fully committed to the mission of
-            creating unforgettable and extraordinary trips to the most beautiful
-            parts of the Carpathians. Our goal is not just to show you the
-            natural wonders of the mountains, but to provide you with a deep
-            immersion in their magical atmosphere.
+            {/* TODO: check if span in span is ok */}
+            <span className="font-normal">a team of enthusiasts</span> who are
+            fully committed to the mission of creating unforgettable and
+            extraordinary trips to the most beautiful parts of the Carpathians.
+            Our goal is not just to show you the natural wonders of the
+            mountains, but to provide you with a deep immersion in their magical
+            atmosphere.
           </span>
-          We believe that nature has the power to inspire, strengthen character
-          and provide new perspectives. Therefore, each of our tours is aimed at
-          unlocking your potential, enriching your spiritual world and creating
-          unforgettable memories.
+          {/* TODO: check if span in span is ok */}
+          <span className="font-normal">We believe</span> that nature has the
+          power to inspire, strengthen character and provide new perspectives.
+          Therefore, each of our tours is aimed at unlocking your potential,
+          enriching your spiritual world and creating unforgettable memories.
         </p>
-        <h3 className="max-w-[180px] mb-10 ">
-          From vacationers to active travelers we have a tour for everyone.
-        </h3>
-        <p>
-          We use methods that are time-tested and proven. Our expert guides with
-          in-depth knowledge of the Carpathian landscapes lead you safely
-          through the mysteries of these mountains.{' '}
+        <p className="max-w-[180px] text-sm font-extralight md:mb-7 md:text-base leading-[20px] xl:text-lg xl:leading-[24px] mb-10 ml-auto tracking-[-0.14px]">
+          <span className="block font-normal uppercase">From vacationers</span>
+          <span className="block font-normal uppercase text-right">
+            to active travelers
+          </span>
+          we have a tour for everyone.
+        </p>
+        <p className="mb-10 text-sm font-extralight md:mb-7 md:text-base leading-[20px] xl:text-lg xl:leading-[24px]">
+          <span className="font-normal">We use methods</span> that are
+          time-tested and proven. Our expert guides with in-depth knowledge of
+          the Carpathian landscapes lead you safely through the mysteries of
+          these mountains.
         </p>
       </div>
     </Section>
